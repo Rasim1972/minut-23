6864da069524215db0e5c48ddb31f2535f34e8a1
Mullahmetov Rasim <rrmullakhmetov@inno.tech>
2023-09-18 11:11:31 +0300

    добавлен новый класс box

---
+++ src/box.java
@@ -0,0 +1,15 @@
+public class box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public box(int size) {
+        this.size = size;
+    }
+}
