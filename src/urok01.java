760614d2fd225926426e023591621dbbc8f04564
Mullahmetov Rasim <rrmullakhmetov@inno.tech>
2023-09-18 13:01:40 +0300

    для 1-го разработчика

--- src/urok01.java
+++ src/urok01.java
@@ -2,8 +2,10 @@ public class urok01 {
     public static void main(String[] args) {
         System.out.println(01);
         System.out.println(02);
-        System.out.println(03);
+        System.out.println(06);
         System.out.println(444);
         System.out.println(555);
+        System.out.println(6666);
+
     }
 }
