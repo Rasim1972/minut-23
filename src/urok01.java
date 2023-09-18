8d3c10fd1d28b167c0faabecebf76b0b3ad857ab
Mullahmetov Rasim <rrmullakhmetov@inno.tech>
2023-09-18 11:18:15 +0300

    оправлен файл урок01

--- src/urok01.java
+++ src/urok01.java
@@ -3,10 +3,7 @@ public class urok01 {
         System.out.println(01);
         System.out.println(02);
         System.out.println(03);
-        System.out.println(04);
-        System.out.println(05);
-
-
-
+        System.out.println(444);
+        System.out.println(555);
     }
 }
